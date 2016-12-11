6e734bc0bd7f6aa212a1328ee567ddf02edbd4e8
jcsto <julio.cesar.mesa.glez@gmail.com>
2016-12-11 01:41:48 -0500

    cambio 1
    Esto fue una prueba de como sería un cambio en un proyecto, puedes aceptar el cambio o declinarlo

--- Variables 2.php
+++ Variables 2.php
@@ -8,6 +8,16 @@
 <?php
 
 echo "hola \\J\\";
+	
+// crear una funcion que calcule el ImC
+	function imc($pesoKg, $estaturaCm)
+	{
+		// $pesoKg     = 65.5; EN kg
+		// $estaturaCm = 172; EN cm
+		$imc;
+		
+		return $imc;
+	}
 
 ?>
 
